ee2315e33afef6fb233ea5d97ebc96d7e475e368
drakobian <drake.bennion@everee.com>
2022-08-02 21:38:12 -0600

    feat: basic implementation

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,26 @@
 use clap::Parser;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::PathBuf;
 
 #[derive(Debug, Parser)]
 struct Cli {
     pattern: String,
 
     #[clap(parse(from_os_str))]
-    path: std::path::PathBuf,
+    path: PathBuf,
 }
 
 fn main() {
     let args = Cli::parse();
-    println!("{:?}", args);
+    
+    let file = File::open(&args.path).expect("could not open the file :(");
+    let content = BufReader::new(file);
+    for line in content.lines() {
+	if let Ok(line) = line {
+        	if line.contains(&args.pattern) {
+            		println!("{}", line);
+        	}
+	}
+    }
 }
