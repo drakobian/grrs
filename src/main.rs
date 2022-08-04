a990f074a15ca1e7531006426659b4e9928bca24
drakobian <drake.bennion@everee.com>
2022-08-03 21:52:21 -0600

    feat: add progress bar and logging demo

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+use anyhow::{Context, Result};
 use clap::Parser;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::path::PathBuf;
+use std::{thread, time};
 
 #[derive(Debug, Parser)]
 struct Cli {
@@ -11,10 +13,19 @@ struct Cli {
     path: PathBuf,
 }
 
-fn main() {
+fn main() -> Result<()> {
     let args = Cli::parse();
     
-    let file = File::open(&args.path).expect("could not open the file :(");
+    let file = File::open(&args.path)
+	.with_context(|| format!("could not open file `{}`", &args.path.display()))?;
+
+    let progress_bar = indicatif::ProgressBar::new(100);
+    for _ in 0..100 {
+        thread::sleep(time::Duration::from_millis(10));
+        progress_bar.inc(1);
+    }
+    progress_bar.finish_with_message("results: ");
+
     let content = BufReader::new(file);
     for line in content.lines() {
 	if let Ok(line) = line {
@@ -23,4 +34,6 @@ fn main() {
         	}
 	}
     }
+
+    Ok(())
 }
